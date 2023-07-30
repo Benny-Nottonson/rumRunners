6251cc774db0d7c36d9f46ce47bd7f0cbfe46c9d
benny <benny.nottonson01@student.csulb.edu>
2023-07-30 10:30:59 -0700

    venue

--- src/components/venues/venues.tsx
+++ src/components/venues/venues.tsx
@@ -8,9 +8,9 @@ export default component$(() => {
       <hr class="border-gray-600" />
       <div class="flex flex-wrap justify-center">
         <ul class="grid gap-2">
-          <VenueItem date="8.13.23" time="3:00 PM - 6:00 PM" name="Hennessy's" adress="143 Main Street Seal Beach, CA 90740"/>
-          <VenueItem date="8.26.23" time="8:00 PM - 11:00 PM" name="Hennessy's" adress="143 Main Street Seal Beach, CA 90740"/>
-          <VenueItem date="10.13.23" time="5:00 PM - 8:00 PM" name="Mother's" adress="16701 CA-1, Sunset Beach, CA 90742"/>
+          <VenueItem date="8.13.23" time="3:00 PM - 6:00 PM" name="Hennessey's" adress="143 Main Street Seal Beach, CA 90740"/>
+          <VenueItem date="8.26.23" time="8:00 PM - 11:00 PM" name="Hennessey's" adress="143 Main Street Seal Beach, CA 90740"/>
+          <VenueItem date="10.13.23" time="5:00 PM - 8:00 PM" name="Mother's Tavern" adress="16701 CA-1, Sunset Beach, CA 90742"/>
         </ul>
       </div>
       <hr class="border-gray-600 mb-16" />
