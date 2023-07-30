5cae93e5c6478d06079c0134f2ee27e6d94d3237
benny <benny.nottonson01@student.csulb.edu>
2023-07-30 10:13:29 -0700

    fixed

--- src/init.mjs
+++ src/init.mjs
@@ -1,21 +1,27 @@
 import { readFileSync, writeFileSync } from "fs";
 import { join } from "path";
+import env from "dotenv";
+
+env.config();
+const api_key = process.env.YOUTUBE_API_KEY;
 
 const channel = "UCxR0eaKXmx2g-5LiiL3VTDQ";
 
 const res = await fetch(
-  `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channel}&maxResults=6&order=date&type=video&key=${process.env.YOUTUBE_API_KEY}`
+  `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channel}&maxResults=6&order=date&type=video&key=${api_key}`
 );
 const json = await res.json();
+
 let final = readFileSync("./youtube.json", "utf8");
+
 if (!json.error) {
   writeFileSync("./youtube.json", JSON.stringify(json));
-  final = json.items;
+  final = json;
 }
 
 writeFileSync(
   join(process.cwd(), "src/feed.ts"),
-  `export const fullFeed: string = ${JSON.stringify(final)};`
+  `export const fullFeed: string = \`${JSON.stringify(final)}\`;`
 );
 
 writeFileSync(join(process.cwd(), "src/feed.json"), JSON.stringify(final));
